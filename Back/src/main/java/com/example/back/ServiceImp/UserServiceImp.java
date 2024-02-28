4e5234b14864636a9badd6a8ceb38e8735527a30
ilefsd <80310278+ilefsd@users.noreply.github.com>
2024-02-28 17:23:22 +0100

    Delete Back directory

--- Back/src/main/java/com/example/back/ServiceImp/UserServiceImp.java
+++
@@ -1,8 +0,0 @@
-package com.example.back.ServiceImp;
-
-import com.example.back.Services.UserService;
-import org.springframework.stereotype.Service;
-
-@Service
-public class UserServiceImp implements UserService {
-}
